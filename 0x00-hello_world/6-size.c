cd4385d90621d9ff824d51c4a86b32a457dcb891
ConradMLinus <conradmlinus@gmail.com>
2023-09-07 10:02:35 -0700

    Correcting my code

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,6 +16,6 @@ int main(void)
 	printf("\nSize of a long int: %u byte(s)", sizeof(long int));
 	printf("\nSize of a long long int: %u byte(s)", sizeof(long long int));
 	printf("\nSize of a float: %u byte(s)", sizeof(float));
-	return (0)
+	return (0);
 
 }
