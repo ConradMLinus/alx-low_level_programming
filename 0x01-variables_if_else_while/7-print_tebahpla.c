d539c24d85c53ad895b6e7a2d99c8e046c0976b7
ConradMLinus <conradmlinus@gmail.com>
2023-09-08 12:25:24 -0700

    Correcting my code

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,17 +1,20 @@
+#include <stdlib.h>
+
 #include <stdio.h>
 
 /**
- * main - Prints all single digit numbers of base 10 starting from 0, followed
- * by a new line.
- * Return: 0
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	int i;
-
-	for (i = 0; i < 10; i++)
-		putchar(i + '0');
-	putchar('\n');
-	return (0);
+int i;
+for (i = 'z'; i >= 'a'; i--)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
 }
