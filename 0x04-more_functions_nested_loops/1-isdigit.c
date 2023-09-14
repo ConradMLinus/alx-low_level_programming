461fcd7af74677982a327b8e974dcecbf0698a54
ConradMLinus <conradmlinus@gmail.com>
2023-09-14 09:55:56 -0700

    Correction

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,13 +1,14 @@
 #include "main.h"
 #include <stdio.h>
 
+
 /**
  * _isdigit - checks if a character is digit
  * @c: carrier variable
  * Return: 1 if true, 0 if false
  */
 
-char _isdigit(char c)
+int _isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
 		return (1);
