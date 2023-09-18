1998c38b5a4134f1c0afb335e742e8c0914e0baa
ConradMLinus <conradmlinus@gmail.com>
2023-09-18 11:24:03 -0700

    Correcting code

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,27 +1,29 @@
 #include <stdio.h>
 
-void puts_half(char *str)
-{
-	int length = 0;
-	int i;
+void puts_half(char *str) {
+    int length = 0;
+    int i;
+    int start_index;
 
-	while (str[length] != '\0')
-	{
-		length++;
-	}
+    // Calculate the length of the string
+    while (str[length] != '\0') {
+        length++;
+    }
 
-	start_index = (length + 1) / 2;
-	for (i = start_index; i < length; i++)
-	{
-		putchar(str[i]);
-	}
+    // Calculate the starting index for the second half
+    start_index = (length + 1) / 2;
 
-	putchar('\n');
+    // Print the second half of the string
+    for (i = start_index; i < length; i++) {
+        putchar(str[i]);
+    }
+
+    // Print a new line
+    putchar('\n');
 }
 
-int main(void)
-{
-	char *str = "0123456789";
-	puts_half(str);
-	return (0);
+int main(void) {
+    char *str = "0123456789";
+    puts_half(str);
+    return (0);
 }
