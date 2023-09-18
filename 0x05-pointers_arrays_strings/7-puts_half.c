d6b9831551f2d8a96d2697f533141fb0f504ca99
ConradMLinus <conradmlinus@gmail.com>
2023-09-18 11:25:56 -0700

    Correcting code

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -5,20 +5,20 @@ void puts_half(char *str) {
     int i;
     int start_index;
 
-    // Calculate the length of the string
+    
     while (str[length] != '\0') {
         length++;
     }
 
-    // Calculate the starting index for the second half
+    
     start_index = (length + 1) / 2;
 
-    // Print the second half of the string
+    
     for (i = start_index; i < length; i++) {
         putchar(str[i]);
     }
 
-    // Print a new line
+    
     putchar('\n');
 }
 
