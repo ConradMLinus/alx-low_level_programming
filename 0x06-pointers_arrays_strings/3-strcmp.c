8c0b13afd8ec9d726aa8995bde757e9f2dd01445
ConradMLinus <conradmlinus@gmail.com>
2023-09-21 13:11:35 -0700

    Correcting my code

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -25,9 +25,9 @@ int main(void) {
   char s1[] = "Hello";
   char s2[] = "World!";
 
-  printf("%d\n", _strcmp(s1, s2)); // Should print a negative number
-  printf("%d\n", _strcmp(s2, s1)); // Should print a positive number
-  printf("%d\n", _strcmp(s1, s1)); // Should print 0
+  printf("%d\n", _strcmp(s1, s2));
+  printf("%d\n", _strcmp(s2, s1));
+  printf("%d\n", _strcmp(s1, s1));
 
   return (0);
 }
