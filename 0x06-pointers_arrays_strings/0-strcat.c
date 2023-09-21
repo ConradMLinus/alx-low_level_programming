4915264f32e370451660fd245a5c3b401fce2776
ConradMLinus <conradmlinus@gmail.com>
2023-09-21 12:42:49 -0700

    Correcting my file

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,23 +9,12 @@
 
 char *_strcat(char *dest, char *src)
 {
-	char *dest_end = dest;
+	int index = 0;
+	int dest_len = 0;
 
-	while (*dest_end != '\0')
-	{
-		dest_end++;
-	}
-
-	while (*src != '\0')
-	{
-		*dest_end = *src;
-		dest_end++;
-		src++;
-	}
-
-	*dest_end = '\0';
-
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
 	return (dest);
-
-
 }
