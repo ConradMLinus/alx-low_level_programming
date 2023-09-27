5f9ac61273a1626647bcea0f80492f077af7fd5c
ConradMLinus <conradmlinus@gmail.com>
2023-09-27 09:40:24 -0700

    Correcting my code

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,14 +5,11 @@
  * @s: The string to be printed.
  */
 
-void _puts_recursion(char *s)
-{
-	if (*s == '\0'
-			{
-			_putchar('\n');
-			return;
-			}
+void _puts_recursion(char * s) {
+    if ( * s == '\0' {
+        _putchar('\n');
+        return;
+      }
 
-			_putchar(*s);
-			_puts_recursion(s + 1);
-}
+      _putchar( * s); _puts_recursion(s + 1);
+    }
