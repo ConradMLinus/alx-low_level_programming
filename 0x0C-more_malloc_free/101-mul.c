22e9efa442850482badc7fcda4edf0b934fc7443
ConradMLinus <conradmlinus@gmail.com>
2023-10-05 08:06:01 -0700

    Code correction

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -25,17 +25,3 @@ void _print(char *str, int l)
 	_putchar('\n');
 	free(str);
 }
-
-/**
- * mul - multiplies a char with a string and places the answer into dest
- * @n: char to multiply
- * @num: string to multiply
- * @num_index: last non NULL index of num
- * @dest: destination of multiplication
- * @dest_index: highest index to start addition
- *
- * Return: pointer to dest, or NULL on failure
- */
-char *mul(char n, char *num, int num_index, char *dest, int dest_index)
-{
-	int j, k, mul, mulrem, add, addrem;
