633659983117fc21ff16c9fa4218c39c9f8e9b9d
ConradMLinus <conradmlinus@gmail.com>
2023-10-09 09:09:25 -0700

    a new type struct dog with the following elements

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * init_dog - Initializes a variable of type struct dog.
+ * @d: The dog to be initialized.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ */
+
+
+
+int main(void)
+{
+	struct dog my_dog;
+
+	my_dog.name = "Poppy";
+	my_dog.age = 3.5;
+	my_dog.owner = "Bob";
+	printf("My name is %s, and I am %.1f :") - Woof!\n", my_dog.name, my_dog.age);
+
+	return (0);
+}
